dfac24295826798c8b461f033b36543084fff400
nathanoakes1995 <o014892c@student.staffs.ac.uk>
2014-12-10 19:01:39 +0000

    Another commit...
    Sorry, it won't let me skip this one and it doesn't seem to be anything
    harmful.

--- FlappyBird/Enemy.cs
+++ FlappyBird/Enemy.cs
@@ -12,19 +12,29 @@ namespace FlappyBird
 	public class Enemy
 	{
 		public Vector2 position;
-		public TextureManager textureManager;
+		private SpriteUV 	sprites;
+		private TextureInfo	textureInfo;
+		private float		width;
 		
 		public Enemy (Scene scene)
 		{
-			Initialise (scene);	
-		}
-		public void Initialise(Scene pointerScene)
-		{
-			position = new Vector2(100.0f,100.0f);
-			textureManager = new TextureManager("/Application/textures/GenericTexture.png", pointerScene);
+			sprites	= new SpriteUV();
+			position = new Vector2(100,100);
+			textureInfo  		= new TextureInfo("/Application/textures/GenericTexture.png");
+			//Left
+			sprites 			= new SpriteUV(textureInfo);
+			sprites.Quad.S 	= textureInfo.TextureSizef;
+			sprites.Position= position;
 			
+			//Get sprite bounds.
+			Bounds2 b = sprites.Quad.Bounds2();
+			width     = b.Point10.X;
 			
+			//Add to the current scene.
+			
+				scene.AddChild(sprites);
 		}
+		
 		
 		public void Update()
 		{
