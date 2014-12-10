45f2b412990c0ae28a411d7e313c7fe82af6d677
ProfessorTenebrae <tamegamecat@hotmail.co.uk>
2014-12-10 18:19:13 +0000

    Textures
    files

--- FlappyBird/Background.cs
+++ FlappyBird/Background.cs
@@ -11,41 +11,87 @@ namespace FlappyBird
 	public class Background
 	{	
 		//Private variables.
-		private SpriteUV[] 	sprites;
-		private TextureInfo	textureInfo;
-		private float		width;
-		
+		private 	SpriteUV 	background;
+		private	 	TextureInfo	textureInfo;
+		private 	float		width;
+		//private	int			saturation;
+
 		//Public functions.
 		public Background (Scene scene)
 		{
-			sprites	= new SpriteUV[3];
+			background	= new SpriteUV();
 			
-			textureInfo  		= new TextureInfo("/Application/textures/background.png");
-			//Left
-			sprites[0] 			= new SpriteUV(textureInfo);
-			sprites[0].Quad.S 	= textureInfo.TextureSizef;
-			//Middle
-			sprites[1] 			= new SpriteUV(textureInfo);
-			sprites[1].Quad.S 	= textureInfo.TextureSizef;
-			//Right
-			sprites[2] 			= new SpriteUV(textureInfo);
-			sprites[2].Quad.S 	= textureInfo.TextureSizef;
+			textureInfo	= new TextureInfo("/Application/textures/background(0%).png");
 			
-			//Get sprite bounds.
-			Bounds2 b = sprites[0].Quad.Bounds2();
+			background			= new SpriteUV(textureInfo);
+			background.Quad.S	= textureInfo.TextureSizef;
+			
+			//Get background bounds.
+			Bounds2 b = background.Quad.Bounds2();
 			width     = b.Point10.X;
 			
+			//saturation = 0;
+			
+			//170959
+			
 			//Add to the current scene.
-			foreach(SpriteUV sprite in sprites)
-				scene.AddChild(sprite);
+			scene.AddChild(background);
+			
+			//Update();
 		}	
 		
+		//public void Update()
+		//{
+			//if (saturation == 0)		
+			//{
+				//textureInfo	= new TextureInfo("/Application/textures/background(0%).png");
+			//}
+			//else if (saturation == -1)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-10%).png");
+			//}
+			//else if (saturation == -2)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-20%).png");
+			//}
+			//else if (saturation == -3)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-30%).png");
+			//}
+			//else if (saturation == -4)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-40%).png");
+			//}
+			//else if (saturation == -5)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-50%).png");
+			//}
+			//else if (saturation == -6)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-60%).png");
+			//}
+			//else if (saturation == -7)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-70%).png");
+			//}
+			//else if (saturation == -8)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-80%).png");
+			//}
+			//else if (saturation == -9)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-90%).png");
+			//}
+			//else if (saturation == -10)
+			//{
+			//	textureInfo	= new TextureInfo("/Application/textures/background(-100%).png");
+			//}
+		//}
+	
 		public void Dispose()
 		{
 			textureInfo.Dispose();
 		}
 	}
 }
-
-
 
